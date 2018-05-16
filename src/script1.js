fd4b96b2fb51a36c72b8f9a2a35aeb59e07f5c37
ibaryshnikov <baryshnikov.il@gmail.com>
2018-05-17 00:19:32 +0300

    initial commit

---
+++ src/script1.js
@@ -0,0 +1,7 @@
+console.log('loaded desu')
+const script = document.currentScript
+console.log(script)
+const id = script.dataset.targetId
+console.log(' script id is', id)
+const element = document.getElementById(id)
+element.innerHTML += ' script loaded'
